24ffc86c3bc7891eda150a7072b72906934110b3
M6K4N9R <zubchevskiy@gmail.com>
2024-05-28 15:51:47 +0200

    Final

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 
 const StyledNavBar = styled.nav`
-  // margin: 20px;
+  position: sticky;
+  top: 0;
   // padding: 20px;
   // border: 2px solid black;
   background-color: #d89e39;
   font-size: 25px;
   color: ${({ $isActive }) => ($isActive ? "#f6834f" : "black")};
+  z-index: 10;
 `;
 
 const StyledList = styled.ul`
@@ -25,7 +27,7 @@ const styledListElement = styled.li``;
 export default function Navigation() {
   const router = useRouter();
   return (
-    <StyledNavBar className="styled-navbar">
+    <StyledNavBar>
       <StyledList>
         <styledListElement className="nav-lists">
           <Link href="/" $isActive={router.pathname === "/"}>
